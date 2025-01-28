20702a063d082308edd4f48ee83eb3e4aabb41c1
Matheus Corniani <matheuscorniani@gmail.com>
2025-01-27 21:13:02 -0300

    feat: adicionada pagina de pagamento

---
+++ app/payment/page.tsx
@@ -0,0 +1,14 @@
+export default function Payment() {
+    return (
+            <div className="max-container h-[90vh] bg-slate-950 md:rounded-xl my-10">
+                <div className="flex justify-between h-full">
+                    <div className="flex-1 bg-white m-2 p-4 rounded-lg">
+                        
+                    </div>
+                    <div className="flex-1 bg-white m-2 p-4 rounded-lg">
+                        
+                    </div>
+                </div>
+            </div>
+    )
+}
