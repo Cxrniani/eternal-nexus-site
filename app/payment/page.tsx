fcfa8005245c144342c26507a27846caf39991f3
Matheus Corniani <matheuscorniani@gmail.com>
2025-01-27 22:38:15 -0300

    feat: add embbed checkout
    COM ERROS
    depois separar API do front-end com python :D

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,14 +1,8 @@
+import Checkout from "@/components/Checkout";
+import EmbeddedCheckoutButton from "@/components/EmbeddedCheckoutButton";
+
 export default function Payment() {
     return (
-            <div className="max-container h-[90vh] bg-slate-950 md:rounded-xl my-10">
-                <div className="flex justify-between h-full">
-                    <div className="flex-1 bg-white m-2 p-4 rounded-lg">
-                        
-                    </div>
-                    <div className="flex-1 bg-white m-2 p-4 rounded-lg">
-                        
-                    </div>
-                </div>
-            </div>
+        <EmbeddedCheckoutButton/>
     )
 }
